56a3cfb4c9c4aca8fb200921b326bbd2f6d2e257
hsinkoff <hsinkoff@hotmail.com>
2017-01-11 13:29:30 -0600

    udpate routes to use route helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'kash#index'
-  get 'complete/:id', to: 'kash#complete'
-  get 'callback/:id', to: 'kash#callback'
-  get 'cancel/:id', to: 'kash#cancel'
+  get 'complete/:id', as: :complete, to: 'kash#complete'
+  get 'callback/:id', as: :callback, to: 'kash#callback'
+  get 'cancel/:id', as: :cancel, to: 'kash#cancel'
 end
