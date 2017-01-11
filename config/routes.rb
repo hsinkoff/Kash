da4a032089ebc3741e4fbc4b37f5d86ff3c0b974
hsinkoff <hsinkoff@hotmail.com>
2017-01-11 14:45:15 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'kash#index'
   get 'complete/:id', as: :complete, to: 'kash#complete'
-  get 'callback/:id', as: :callback, to: 'kash#callback'
+  post 'callback/:id', as: :callback, to: 'kash#callback'
   get 'cancel/:id', as: :cancel, to: 'kash#cancel'
 end
